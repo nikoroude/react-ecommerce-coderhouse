34e82e0ad398d9dabf16c51cc23d9d4184809d1a
nikoroude <nikoroude@gmail.com>
2022-09-13 22:31:51 -0300

    context

--- src/App.js
+++ src/App.js
@@ -7,37 +7,43 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Nosotros from './components/Nosotros/Nosotros';
 import Contacto from './components/Contacto/Contacto';
 import Cart from './components/Cart/Cart';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-
+import { CartContext } from './context/CartContext'
+// import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 
 
 function App() {
+
+  
+
   return (
+    <CartContext.Provider value={ {} }>
 
-    <BrowserRouter>
+      <BrowserRouter>
 
-      <NavBar title="e-commerce" />
+        <NavBar title="e-commerce" />
 
-      <Routes id='Routes'>
-        <Route path='*' element={ <ListProducts title="OFERTAS IMPERDIBLES DE LA SEMANA" /> } />
-        
-        <Route path='/Productos/:categoryId' element={<ListProducts />}/>
-        <Route path='/Item/:itemId' element={<ItemDetailContainer />}/>
-        
-        <Route path='/Nosotros' element={ <Nosotros /> }/>
-        <Route path='/Contacto' element={ <Contacto /> }/>
-        <Route path='/Cart' element={ <Cart /> }/>
-        <Route path='/Producto' element={ <ItemDetailContainer idProducto={6} /> } />
-        <Route path='*' element={ <Navigate to="/" />}/>
-      </Routes>
-        
+        <Routes id='Routes'>
+          <Route path='*' element={<ListProducts title="OFERTAS IMPERDIBLES DE LA SEMANA" />} />
+
+          <Route path='/Productos/:categoryId' element={<ListProducts />} />
+          <Route path='/Item/:itemId' element={<ItemDetailContainer />} />
+
+          <Route path='/Nosotros' element={<Nosotros />} />
+          <Route path='/Contacto' element={<Contacto />} />
+          <Route path='/Cart' element={<Cart />} />
+          {/* <Route path='/Producto' element={<ItemDetailContainer idProducto={6} />} /> */}
+          <Route path='*' element={<Navigate to="/" />} />
+
+        </Routes>
 
 
-      <Footer id='App-footer'/>
 
-    </BrowserRouter>
+        <Footer id='App-footer' />
 
+      </BrowserRouter>
+
+    </CartContext.Provider>
   );
 }
 
